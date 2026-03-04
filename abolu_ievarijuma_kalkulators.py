1a533392fb669371962a81ded7333d6f39f261d9
Ayanami <saltinuk160508@gmail.com>
2026-03-04 14:40:22 +0200

    fails ar aprekeniem

--- abolu_ievarijuma_kalkulators.py
+++ abolu_ievarijuma_kalkulators.py
@@ -3,8 +3,19 @@ def ievade()
     cuk_cen=float(input("Ievādi cukura daudzumu (EUR): "))
 
 def cuk_daudz()  
-def kop_iz() 
-def gal_masa()  
+aprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekini
+
+def kop_iz() aprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekini
+aprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekini
+aprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekini
+v
+aprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekini
+aprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekini
+
+def gal_masa()  aprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekini
+aprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekiniaprekini 
+
+def izvade() 
 
 
 
