2a513c55914024db88bd018bef598ebef8eaf948
Ayanami <saltinuk160508@gmail.com>
2026-03-04 14:39:45 +0200

    fails datu ievadīsana

--- abolu_ievarijuma_kalkulators.py
+++ abolu_ievarijuma_kalkulators.py
@@ -1,5 +1,10 @@
 def ievade()
-def cuk_daudz()
-def kop_iz()
-def gal_masa()
-def izvade()
+    ab_svars=float(input("Ievādi ābola svaru (kg): "))
+    cuk_cen=float(input("Ievādi cukura daudzumu (EUR): "))
+
+def cuk_daudz()  
+def kop_iz() 
+def gal_masa()  
+
+
+
